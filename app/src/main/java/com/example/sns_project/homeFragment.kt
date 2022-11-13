b5d7d2f42154b244a2e291741d13d981667f5644
ghkd6030 <hjb010603@naver.com>
2022-11-13 15:50:29 +0900

    ghkd6030 commit

--- app/src/main/java/com/example/sns_project/homeFragment.kt
+++ app/src/main/java/com/example/sns_project/homeFragment.kt
@@ -43,6 +43,11 @@ class homeFragment : Fragment(R.layout.fragment_home) {
         binding.buttonPost.setOnClickListener {
             startActivity(Intent(activity,PostActivity::class.java)) //버튼클릭 시 포스트로 이동
         }
+
+        binding.button2.setOnClickListener{
+            startActivity(Intent(activity,CommentActivity::class.java))
+        }
+
         //DB에서 가져와서 리사이클러뷰에 표시
 
         binding.recyclerView.layoutManager = LinearLayoutManager(context)
